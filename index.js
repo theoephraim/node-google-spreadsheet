5ff67dcfd90c03fcebf845b3ba89fa483154b8ce
Matt Fullerton <matt.fullerton@gmail.com>
2013-10-02 10:08:45 +0200

    Fixes #1 by modifying regex to cope with new lines in the text (http://stackoverflow.com/questions/1068280/javascript-regex-multiline-flag-doesnt-work)

--- index.js
+++ index.js
@@ -71,9 +71,7 @@ module.exports = function( ss_key, auth_id ){
 		self.makeFeedRequest( ["list", ss_key, worksheet_id], 'GET', query, function(err, data, xml) {
 			if ( err ) return cb( err );
 			// gets the raw xml for each entry -- this is passed to the row object so we can do updates on it later
-                        // Credit http://stackoverflow.com/questions/9745874/javascript-replacing-all-instances-of-new-line-character-ascii-13-with-r-n
-                        var xml = xml.replace(/\n/g, " "); //This can't cope with new lines, it seems, so we get rid of them
-			var entries_xml = xml.match(/<entry[^>]*>(.*?)<\/entry>/g);
+            var entries_xml = xml.match(/<entry[^>]*>(.[\s\S]*?)<\/entry>/g);
 			var rows = [];
 			var entries = forceArray( data.entry );
 			var i=0;
