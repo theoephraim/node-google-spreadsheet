572af213ea81a454761989463fde535c946868de
Olivier Penhoat <openhoat@gmail.com>
2013-11-11 13:11:13 +0100

    add bearer feature

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var GOOGLE_FEED_URL = "https://spreadsheets.google.com/feeds/";
 
 // NOTE: worksheet IDs start at 1
 
-module.exports = function (ss_key, auth_id) {
+module.exports = function( ss_key, auth_id ){
   var google_auth;
   var self = this;
 
@@ -18,34 +18,34 @@ module.exports = function (ss_key, auth_id) {
     explicitRoot: false,
   });
 
-  if (!ss_key) {
+  if ( !ss_key ) {
     throw new Error("Spreadsheet key not provided.");
   }
-  if (auth_id) {
+  if ( auth_id ){
     google_auth = auth_id;
   }
 
-  this.setAuth = function (username, password, cb) {
+  this.setAuth = function( username, password, cb ){
     var new_auth = new GoogleClientLogin({
       email: username,
       password: password,
       service: 'spreadsheets',
       accountType: GoogleClientLogin.accountTypes.google
     })
-    new_auth.on(GoogleClientLogin.events.login, function () {
+    new_auth.on(GoogleClientLogin.events.login, function(){
       google_auth = new_auth.getAuthId();
-      cb(null, new_auth);
+      cb( null, new_auth );
     })
-    new_auth.on(GoogleClientLogin.events.error, function (err) {
-      cb(err);
+    new_auth.on(GoogleClientLogin.events.error, function(err){
+      cb( err );
     })
     new_auth.login();
   }
 
 
-  this.getInfo = function (cb) {
-    self.makeFeedRequest(["worksheets", ss_key], 'GET', null, function (err, data, xml) {
-      if (err) return cb(err);
+  this.getInfo = function( cb ){
+    self.makeFeedRequest( ["worksheets", ss_key], 'GET', null, function(err, data, xml) {
+      if ( err ) return cb( err );
       var ss_data = {
         title: data.title["_"],
         updated: data.updated,
@@ -53,77 +53,76 @@ module.exports = function (ss_key, auth_id) {
         worksheets: []
       }
       var worksheets = forceArray(data.entry);
-      worksheets.forEach(function (ws_data) {
-        ss_data.worksheets.push(new SpreadsheetWorksheet(self, ws_data));
+      worksheets.forEach( function( ws_data ) {
+        ss_data.worksheets.push( new SpreadsheetWorksheet( self, ws_data ) );
       })
-      cb(null, ss_data);
+      cb( null, ss_data );
     });
   }
-  this.getRows = function (worksheet_id, opts, query, cb) {
+  this.getRows = function( worksheet_id, opts, query, cb ){
     // the first row is used as titles/keys and is not included
 
     // opts is optional
-    if (typeof( opts ) == 'function') {
+    if ( typeof( opts ) == 'function' ){
       cb = opts;
       opts = {};
       query = null;
       // so is query
-    } else if (typeof( query ) == 'function') {
+    } else if ( typeof( query ) == 'function' ){
       cb = query;
       query = null;
     }
 
-    if (opts.start) query["start-index"] = opts.start;
-    if (opts.num) query["max-results"] = opts.num;
-    if (opts.orderby) query["orderby"] = opts.orderby;
-    if (opts.reverse) query["reverse"] = opts.reverse;
+    if ( opts.start ) query["start-index"] = opts.start;
+    if ( opts.num ) query["max-results"] = opts.num;
+    if ( opts.orderby ) query["orderby"] = opts.orderby;
+    if ( opts.reverse ) query["reverse"] = opts.reverse;
 
-    self.makeFeedRequest(["list", ss_key, worksheet_id], 'GET', query, function (err, data, xml) {
-      if (err) return cb(err);
+    self.makeFeedRequest( ["list", ss_key, worksheet_id], 'GET', query, function(err, data, xml) {
+      if ( err ) return cb( err );
 
       // gets the raw xml for each entry -- this is passed to the row object so we can do updates on it later
       var entries_xml = xml.match(/<entry[^>]*>([\s\S]*?)<\/entry>/g);
       var rows = [];
-      var entries = forceArray(data.entry);
-      var i = 0;
-      entries.forEach(function (row_data) {
-        rows.push(new SpreadsheetRow(self, row_data, entries_xml[ i++ ]));
+      var entries = forceArray( data.entry );
+      var i=0;
+      entries.forEach( function( row_data ) {
+        rows.push( new SpreadsheetRow( self, row_data, entries_xml[ i++ ] ) );
       });
       cb(null, rows);
     });
 
   }
 
-  this.addRow = function (worksheet_id, data, cb) {
-    if (!worksheet_id) throw new Error("Worksheet not specified.");
+  this.addRow = function( worksheet_id, data, cb ){
+    if( !worksheet_id ) throw new Error("Worksheet not specified.");
 
     var data_xml = '<entry xmlns="http://www.w3.org/2005/Atom" xmlns:gsx="http://schemas.google.com/spreadsheets/2006/extended">' + "\n";
-    Object.keys(data).forEach(function (key) {
-      if (key != 'id' && key != 'title' && key != 'content' && key != '_links') {
-        data_xml += '<gsx:' + xmlSafeColumnName(key) + '>' + xmlSafeValue(data[key]) + '</gsx:' + xmlSafeColumnName(key) + '>' + "\n"
+    Object.keys(data).forEach(function(key) {
+      if (key != 'id' && key != 'title' && key != 'content' && key != '_links'){
+        data_xml += '<gsx:'+ xmlSafeColumnName(key) + '>' + xmlSafeValue(data[key]) + '</gsx:'+ xmlSafeColumnName(key) + '>' + "\n"
       }
     });
     data_xml += '</entry>';
-    self.makeFeedRequest(["list", ss_key, worksheet_id], 'POST', data_xml, cb);
+    self.makeFeedRequest( ["list", ss_key, worksheet_id], 'POST', data_xml, cb );
   }
 
-  this.makeFeedRequest = function (url_params, method, query_or_data, cb) {
+  this.makeFeedRequest = function( url_params, method, query_or_data, cb ){
     var url;
     var headers = {};
-    if (!cb) cb = function () {
-    };
-    if (typeof(url_params) == 'string') {
+    if (!cb ) cb = function(){};
+    if ( typeof(url_params) == 'string' ) {
       // used for edit / delete requests
       url = url_params;
-    } else if (Array.isArray(url_params)) {
+    } else if ( Array.isArray( url_params )){
       //used for get and post requets
       var visibility = google_auth ? 'private' : 'public';
       var projection = google_auth ? 'full' : 'values';
-      url_params.push(visibility, projection);
+      url_params.push( visibility, projection );
       url = GOOGLE_FEED_URL + url_params.join("/");
     }
 
-    if (google_auth) {
+    if ( google_auth ) {
       if (google_auth.type === 'Bearer') {
         headers.Authorization = 'Bearer ' + google_auth.value;
       } else {
@@ -131,36 +130,36 @@ module.exports = function (ss_key, auth_id) {
       }
     }
 
-    if (method == 'POST' || method == 'PUT') {
+    if ( method == 'POST' || method == 'PUT' ){
       headers['content-type'] = 'application/atom+xml';
     }
 
-    if (method == 'GET' && query_or_data) {
-      url += "?" + querystring.stringify(query_or_data);
+    if ( method == 'GET' && query_or_data ) {
+      url += "?" + querystring.stringify( query_or_data );
     }
 
-    request({
+    request( {
       url: url,
       method: method,
       headers: headers,
       body: method == 'POST' || method == 'PUT' ? query_or_data : null,
-    }, function (err, response, body) {
+    }, function(err, response, body){
       if (err) {
-        return cb(err);
-      } else if (response.statusCode === 401) {
-        return cb(new Error("Invalid authorization key."));
-      } else if (response.statusCode >= 400) {
-        return cb(new Error("HTTP error " + response.statusCode + ": " + http.STATUS_CODES[response.statusCode]));
+        return cb( err );
+      } else if( response.statusCode === 401 ) {
+        return cb( new Error("Invalid authorization key."));
+      } else if ( response.statusCode >= 400 ) {
+        return cb( new Error("HTTP error " + response.statusCode + ": " + http.STATUS_CODES[response.statusCode]));
       }
 
-      if (body) {
-        xml_parser.parseString(body, function (err, result) {
-          if (err) return cb(err);
-          cb(null, result, body);
+      if ( body ){
+        xml_parser.parseString(body, function(err, result){
+          if ( err ) return cb( err );
+          cb( null, result, body );
         });
       } else {
-        if (err) cb(err);
-        else cb(null, true);
+        if ( err ) cb( err );
+        else cb( null, true );
       }
 
     })
@@ -168,28 +167,28 @@ module.exports = function (ss_key, auth_id) {
 };
 
 // Classes
-var SpreadsheetWorksheet = function (spreadsheet, data) {
+var SpreadsheetWorksheet = function( spreadsheet, data ){
   var self = this;
-  self.id = data.id.substring(data.id.lastIndexOf("/") + 1);
+  self.id = data.id.substring( data.id.lastIndexOf("/") + 1 );
   self.title = data.title["_"];
   self.rowCount = data['gs:rowCount'];
   self.colCount = data['gs:colCount'];
 
-  this.getRows = function (opts, query, cb) {
-    spreadsheet.getRows(self.id, opts, query, cb);
+  this.getRows = function( opts, query, cb ){
+    spreadsheet.getRows( self.id, opts, query, cb );
   }
-  this.addRow = function (data, cb) {
-    spreadsheet.addRow(self.id, data, cb);
+  this.addRow = function( data, cb ){
+    spreadsheet.addRow( self.id, data, cb );
   }
 }
 
-var SpreadsheetRow = function (spreadsheet, data, xml) {
+var SpreadsheetRow = function( spreadsheet, data, xml ){
   var self = this;
   self['_xml'] = xml;
-  Object.keys(data).forEach(function (key) {
+  Object.keys(data).forEach(function(key) {
     var val = data[key];
-    if (key.substring(0, 4) === "gsx:") {
-      if (typeof val === 'object' && Object.keys(val).length === 0) {
+    if(key.substring(0, 4) === "gsx:") {
+      if(typeof val === 'object' && Object.keys(val).length === 0) {
         val = null;
       }
       if (key == "gsx:") {
@@ -202,17 +201,17 @@ var SpreadsheetRow = function (spreadsheet, data, xml) {
         self[key] = val;
       } else if (val['_']) {
         self[key] = val['_'];
-      } else if (key == 'link') {
+      } else if ( key == 'link' ){
         self['_links'] = [];
-        val = forceArray(val);
-        val.forEach(function (link) {
+        val = forceArray( val );
+        val.forEach( function( link ){
           self['_links'][ link['$']['rel'] ] = link['$']['href'];
         });
       }
     }
   }, this);
 
-  self.save = function (cb) {
+  self.save = function( cb ){
     /*
      API for edits is very strict with the XML it accepts
      So we just do a find replace on the original XML.
@@ -222,32 +221,32 @@ var SpreadsheetRow = function (spreadsheet, data, xml) {
     var data_xml = self['_xml'];
     // probably should make this part more robust?
     data_xml = data_xml.replace('<entry>', "<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gsx='http://schemas.google.com/spreadsheets/2006/extended'>");
-    Object.keys(self).forEach(function (key) {
-      if (key.substr(0, 1) != '_' && typeof( self[key] == 'string')) {
-        data_xml = data_xml.replace(new RegExp('<gsx:' + xmlSafeColumnName(key) + ">([\\s\\S]*?)</gsx:" + xmlSafeColumnName(key) + '>'), '<gsx:' + xmlSafeColumnName(key) + '>' + xmlSafeValue(self[key]) + '</gsx:' + xmlSafeColumnName(key) + '>');
+    Object.keys( self ).forEach( function(key) {
+      if (key.substr(0,1) != '_' && typeof( self[key] == 'string') ){
+        data_xml = data_xml.replace( new RegExp('<gsx:'+xmlSafeColumnName(key)+">([\\s\\S]*?)</gsx:"+xmlSafeColumnName(key)+'>'), '<gsx:'+xmlSafeColumnName(key)+'>'+ xmlSafeValue(self[key]) +'</gsx:'+xmlSafeColumnName(key)+'>');
       }
     });
-    spreadsheet.makeFeedRequest(self['_links']['edit'], 'PUT', data_xml, cb);
+    spreadsheet.makeFeedRequest( self['_links']['edit'], 'PUT', data_xml, cb );
   }
-  self.del = function (cb) {
-    spreadsheet.makeFeedRequest(self['_links']['edit'], 'DELETE', null, cb);
+  self.del = function( cb ){
+    spreadsheet.makeFeedRequest( self['_links']['edit'], 'DELETE', null, cb );
   }
 }
 
 //utils
-var forceArray = function (val) {
-  if (Array.isArray(val)) return val;
-  if (!val) return [];
+var forceArray = function(val) {
+  if ( Array.isArray( val ) ) return val;
+  if ( !val ) return [];
   return [ val ];
 }
-var xmlSafeValue = function (val) {
-  if (val == null) return '';
+var xmlSafeValue = function(val){
+  if ( val == null ) return '';
   return String(val).replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
     .replace(/"/g, '&quot;');
 }
-var xmlSafeColumnName = function (val) {
+var xmlSafeColumnName = function(val){
   if (!val) return '';
   return String(val).replace(/\s+/g, '')
     .toLowerCase();
