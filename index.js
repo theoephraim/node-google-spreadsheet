4396fa54f5fdae93487437dc27d874e20cac50ba
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-01-20 17:25:23 +0100

    only enumerate header fields & clean-up

--- index.js
+++ index.js
@@ -350,54 +350,71 @@ var SpreadsheetWorksheet = function( spreadsheet, data ){
 }
 
 var SpreadsheetRow = function( spreadsheet, data, xml ){
-  var self = this;
-  self['_xml'] = xml;
   Object.keys(data).forEach(function(key) {
     var val = data[key];
+
     if(key.substring(0, 4) === "gsx:") {
-      if(typeof val === 'object' && Object.keys(val).length === 0) {
+      if(typeof val === 'object' && !Object.keys(val).length) {
         val = null;
       }
-      if (key == "gsx:") {
-        self[key.substring(0, 3)] = val;
+
+      if (key === "gsx:") {
+        this.gsx = val;
       } else {
-        self[key.substring(4)] = val;
+        this[key.substring(4)] = val;
       }
     } else {
-      if (key == "id") {
-        self[key] = val;
-      } else if (val['_']) {
-        self[key] = val['_'];
-      } else if ( key == 'link' ){
-        self['_links'] = [];
-        val = forceArray( val );
-        val.forEach( function( link ){
-          self['_links'][ link['$']['rel'] ] = link['$']['href'];
-        });
+      if (key === "id") {
+        this[key] = val;
+      } else if (val._) {
+        this[key] = val._;
+      } else if ( key === 'link' ){
+        this._links = {};
+
+        forceArray( val ).forEach( function( link ){
+          var $ = link.$;
+          this._links[$.rel] = $.href;
+        }, this);
       }
     }
   }, this);
 
-  self.save = function( cb ){
-    /*
-    API for edits is very strict with the XML it accepts
-    So we just do a find replace on the original XML.
-    It's dumb, but I couldnt get any JSON->XML conversion to work reliably
-    */
+  Object.defineProperties(this,
+  {
+    _xml: {value: xml},
 
-    var data_xml = self['_xml'];
-    // probably should make this part more robust?
-    data_xml = data_xml.replace('<entry>', "<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gsx='http://schemas.google.com/spreadsheets/2006/extended'>");
-      Object.keys( self ).forEach( function(key) {
-        if (key.substr(0,1) != '_' && typeof( self[key] == 'string') ){
-          data_xml = data_xml.replace( new RegExp('<gsx:'+xmlSafeColumnName(key)+">([\\s\\S]*?)</gsx:"+xmlSafeColumnName(key)+'>'), '<gsx:'+xmlSafeColumnName(key)+'>'+ xmlSafeValue(self[key]) +'</gsx:'+xmlSafeColumnName(key)+'>');
-        }
-    });
-    spreadsheet.makeFeedRequest( self['_links']['edit'], 'PUT', data_xml, cb );
-  }
-  self.del = function( cb ){
-    spreadsheet.makeFeedRequest( self['_links']['edit'], 'DELETE', null, cb );
-  }
+    id:           {enumerable: false},
+    'app:edited': {enumerable: false},
+    _links:       {enumerable: false},
+
+    save:
+    {
+      value: function( cb ){
+        /*
+        API for edits is very strict with the XML it accepts
+        So we just do a find replace on the original XML.
+        It's dumb, but I couldnt get any JSON->XML conversion to work reliably
+        */
+
+        var data_xml = this._xml;
+        // probably should make this part more robust?
+        data_xml = data_xml.replace('<entry>', "<entry xmlns='http://www.w3.org/2005/Atom' xmlns:gsx='http://schemas.google.com/spreadsheets/2006/extended'>");
+          Object.keys( this ).forEach( function(key) {
+            if (key.substr(0,1) != '_' && typeof this[key] === 'string'){
+              data_xml = data_xml.replace( new RegExp('<gsx:'+xmlSafeColumnName(key)+">([\\s\\S]*?)</gsx:"+xmlSafeColumnName(key)+'>'), '<gsx:'+xmlSafeColumnName(key)+'>'+ xmlSafeValue(self[key]) +'</gsx:'+xmlSafeColumnName(key)+'>');
+            }
+        });
+        spreadsheet.makeFeedRequest( this._links.edit, 'PUT', data_xml, cb );
+      }
+    },
+
+    del:
+    {
+      value: function( cb ){
+        spreadsheet.makeFeedRequest( this._links.edit, 'DELETE', null, cb );
+      }
+    }
+  })
 }
 
 var SpreadsheetCell = function( spreadsheet, worksheet_id, data ){
