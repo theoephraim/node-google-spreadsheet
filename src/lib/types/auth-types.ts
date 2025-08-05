8e4e4f755323a425803f166735f413e33b4f074e
Theo Ephraim <theo@dmno.dev>
2025-08-05 00:41:22 -0700

    fix peer dep type issue

--- src/lib/types/auth-types.ts
+++ src/lib/types/auth-types.ts
@@ -1,11 +1,7 @@
-// optional peer dependency - this ts-ignore should help when user is not using it
-// @ts-ignore
-import type { Headers } from 'google-auth-library/build/src/auth/oauth2client';
-
 /** single type to handle all valid auth types */
 export type GoogleApiAuth =
   // this simple interface should cover all google-auth-library auth methods
-  | { getRequestHeaders: () => Promise<Headers> }
+  | { getRequestHeaders: () => Promise<any> }
   // used to pass in an API key only
   | { apiKey: string }
   // used to pass in a raw token
